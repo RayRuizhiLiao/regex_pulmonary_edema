989417ef0ec9e24015d4fef5835783bbfbca62c3
Ray <ray@Rays-MacBook-Pro-2.local>
2020-06-23 17:59:54 -0400

    added mention keywords

--- label_reports.py
+++ label_reports.py
@@ -1,16 +1,15 @@
-# import re
 # import csv
 # from shutil import copyfile
-# import sys
+import sys
 import os
 import argparse
-
 import pandas as pd
+import re
 
-# current_path = os.path.dirname(os.path.abspath(__file__))
-# negex_path = os.path.join(current_path, 'negex/negex.python/')
-# sys.path.insert(0, negex_path)
-# import negex
+current_path = os.path.dirname(os.path.abspath(__file__))
+negex_path = os.path.join(current_path, 'negex/negex.python/')
+sys.path.insert(0, negex_path)
+import negex
 
 
 class WordMatch(object):
@@ -96,7 +95,43 @@ class WordMatch(object):
 
 		return self.words_mentioned_negative 
 
-def label_report(report, keywords, affirmed=True):
+
+def label_report(report, severities, keywords, tag='mentioned'):
+	"""
+	Label a given report the pulmonary edema severity the report indicates.
+	The keywords associated with each severity level are also given.
+	The tag can be "mentioned"/"affirmed"/"negated" and will be used in the keyword matching.
+	Note multiple levels (i.e. their keywords) might be detected,
+	and the most severe level will be returned. 
+
+	Returns:
+		label: the pulmonary edema severity of the given report as an integer between 0 and 3.
+		-1 means the report cannot be labeled. 
+		severity_keywords: the keywords that are detected in the report associated with each severity level.
+	"""
+	label = -1
+	severity_keywords = {0:[], 1:[], 2:[], 3:[]}
+
+	sentences = re.split('\.|\:', report)
+	for sentence in sentences:
+		word_match = WordMatch(sentence, keywords)
+		if tag == 'mentioned':
+			keywords_detected = word_match.mention()
+		if tag == 'affirmed':
+			keywords_detected = word_match.mention_positive()
+		if tag == 'negated':
+			keywords_detected = word_match.mention_negative()
+		for i in range(len(severities)):
+			severity = severities[i]
+			keyword = keywords[i]
+			if keywords_detected[keyword] == True:
+				if keyword not in severity_keywords[severity]:
+					severity_keywords[severity].append(keyword)
+				if severity>label:
+					label = severity
+
+	return label, severity_keywords
+
 
 def main(args):
 
@@ -106,14 +141,45 @@ def main(args):
 	df = pd.read_csv(args.affirmed_keywords_path, 
 					 sep="\t")
 	affirmed_keywords = df.to_dict()
+	df = pd.read_csv(args.mentioned_keywords_path, 
+					 sep="\t")
+	mentioned_keywords = df.to_dict()
 
 	for filename in os.listdir(args.report_dir):
 		report_path = os.path.join(args.report_dir, filename)
 		with open(report_path, 'r') as file:
 			report = file.read()
-			print(report)
-	
 
+			severities = [affirmed_keywords['pulmonary_edema_severity'][i] \
+			for i in range(len(affirmed_keywords['pulmonary_edema_severity']))]
+			keywords = [affirmed_keywords['keyword_terms'][i] \
+			for i in range(len(affirmed_keywords['keyword_terms']))]
+			label_a, severity_keywords_a = label_report(report, severities,
+														keywords, tag='affirmed')
+
+			severities = [negated_keywords['pulmonary_edema_severity'][i] \
+			for i in range(len(negated_keywords['pulmonary_edema_severity']))]
+			keywords = [negated_keywords['keyword_terms'][i] \
+			for i in range(len(negated_keywords['keyword_terms']))]
+			label_n, severity_keywords_n = label_report(report, severities,
+														keywords, tag='negated')
+
+			severities = [mentioned_keywords['pulmonary_edema_severity'][i] \
+			for i in range(len(mentioned_keywords['pulmonary_edema_severity']))]
+			keywords = [mentioned_keywords['keyword_terms'][i] \
+			for i in range(len(mentioned_keywords['keyword_terms']))]
+			label_m, severity_keywords_m = label_report(report, severities,
+														keywords, tag='mentioned')
+
+			print(filename)
+			label = max([label_a, label_n, label_m])
+			print(label)
+			if label != -1:
+				relevant_keywords = severity_keywords_a[label]
+				relevant_keywords += severity_keywords_n[label]
+				relevant_keywords += severity_keywords_m[label]
+			print(relevant_keywords)
+	
 
 def past():
 
@@ -241,6 +307,8 @@ if __name__ == '__main__':
 												 'miccai2020', 'keywords_negated.tsv')
 	default_affirmed_keywords_path = os.path.join(current_path, 'keywords',
 												  'miccai2020', 'keywords_affirmed.tsv')
+	default_mentioned_keywords_path = os.path.join(current_path, 'keywords',
+												  'miccai2020', 'keywords_mentioned.tsv')
 	default_report_dir = os.path.join(current_path, 'example_data')
 
 	parser = argparse.ArgumentParser()
@@ -255,6 +323,11 @@ if __name__ == '__main__':
 		default=default_affirmed_keywords_path,
 		help='the .tsv file that has keyword terms for labeling pulmonary edema severity '\
 			 'in an affirmed fashion')
+	parser.add_argument(
+		'--mentioned_keywords_path',
+		default=default_mentioned_keywords_path,
+		help='the .tsv file that has keyword terms for labeling pulmonary edema severity '\
+			 'in a mentioned fashion')
 	parser.add_argument(
 		'--report_dir',
 		default=default_report_dir,
